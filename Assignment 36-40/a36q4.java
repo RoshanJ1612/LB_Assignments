a225894a1688987d9cbfc95ba1178deb2035a48d
Roshan Jagtap <91279369+RoshanJ1612@users.noreply.github.com>
2021-10-27 11:06:20 +0530

    Update a36q4.java

--- Assignment 36-40/a36q4.java
+++ Assignment 36-40/a36q4.java
@@ -1,7 +1,7 @@
 /*4. Write Java program which accept number of rows and number of columns 
 from user and display below pattern.
 Input : iRow = 4 iCol = 5
-Output : 4 4 4 4 4 
+Output : 	 4 4 4 4 4 
 		 3 3 3 3 3 
 		 2 2 2 2 2 
 		 1 1 1 1 1*/
